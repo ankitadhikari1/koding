3e7c7ae97b7e258fd944878b618d815ae9e2a395
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-07-28 17:07:26 +0530

    Time: 0 ms (100%), Space: 18.8 MB (96.87%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+
+private:
+   vector<int>topoSort(int n,vector<int>adj[]){
+        vector<int>ans;
+        vector<int>inDegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto & it : adj[i]){
+                inDegree[it]++;
+            }
+        }
+
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(inDegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto & it :adj[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0){
+                    q.push(it);
+                }
+            }
+
+        }
+
+        return ans;
+    }
+
+
+
+public:
+    bool canFinish(int N, vector<vector<int>>& arr) {
+        vector<int>adj[N];
+        for(auto & it : arr){
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<int>topo = topoSort(N,adj);
+        return topo.size() == N ;
+    }
+};
