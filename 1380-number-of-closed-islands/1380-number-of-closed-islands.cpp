84a00aa52de2a19283860217637a02e060ddd81b
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-03 00:35:38 +0530

    Time: 7 ms (22.88%), Space: 17.6 MB (22.68%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
 
-    void fill(int i,int j,vector<vector<int>>&vis,vector<vector<int>>&grid){
+    void fill(int i,int j,vector<vector<int>>&grid){
         int n = grid.size();
         int m = grid[0].size();
-        vis[i][j] = 1;
         grid[i][j] = 1;
         queue<pair<int,int>>q;
         q.push({i,j});
@@ -20,9 +19,8 @@ public:
             for(int i=0;i<4;i++){
                 int nx = row + dx[i];
                 int ny = col + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && vis[nx][ny] == 0 && grid[nx][ny] == 0){
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 0){
                     grid[nx][ny] = 1;
-                    vis[nx][ny] = 1;
                     q.push({nx,ny});
                 }
             }
@@ -30,57 +28,30 @@ public:
         }
     }
 
-
-    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>&grid){
-        int n = grid.size();
-        int m = grid[0].size();
-        if(row<0 || row>=n || col < 0 || col>=m || vis[row][col]==1 || grid[row][col]==1 ){
-            return;
-        }
-
-        grid[row][col] = 1;
-        vis[row][col] = 1;
-
-        int dx[] = {1,0,-1,0};
-        int dy[] = {0,-1,0,1};
-
-        for(int i=0;i<4;i++){
-            int nx = row + dx[i];
-            int ny = col + dy[i];
-            dfs(nx,ny,vis,grid);
-            
-            
-        }
-
-    }
-
-
-
     int closedIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int j=0;j<m;j++){
             if(grid[0][j]==0)
-            fill(0,j,vis,grid);
+            fill(0,j,grid);
 
             if(grid[n-1][j]==0)
-            fill(n-1,j,vis,grid);
+            fill(n-1,j,grid);
         }
         for(int i =0;i<n;i++){
             if(grid[i][0]==0)
-            fill(i,0,vis,grid);
+            fill(i,0,grid);
 
             if(grid[i][m-1]==0)
-            fill(i,m-1,vis,grid);
+            fill(i,m-1,grid);
         }
 
         int cnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==0 && !vis[i][j]){
+                if(grid[i][j]==0){
                     cnt++;
-                    dfs(i,j,vis,grid);
+                    fill(i,j,grid);
                 }
             }
         }
