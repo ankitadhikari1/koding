66ce060a09838d205db3cb0eccd93ae2429edb27
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-12 14:35:16 +0530

    Time: 0 ms (100%), Space: 22.1 MB (17.66%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -15,7 +15,7 @@ public:
     bool helper(TreeNode* root , long long minVal , long long maxVal){
             if(root==NULL)return true;
             if(root->val <= minVal || root->val >= maxVal)return false;
-            return helper(root->left,minVal,root->val) && helper(root->right,root->val,maxVal);
+            return helper(root->left,minVal,root->val) & helper(root->right,root->val,maxVal);
 
         }
 
