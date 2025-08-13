9a02260fec9cc37d1befaeb3d3a2a914e0f8680b
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-13 15:03:15 +0530

    Time: 0 ms (100%), Space: 16.7 MB (88.55%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,76 +1,47 @@
-class Solution{
+class Solution {
 public:
-
-
-    bool isValidFresh(int row, int col , vector<vector<int>>&grid , vector<vector<int>>&vis){
-      int n = grid.size();
-      int m = grid[0].size();
-      if(row<0 || row>= n)return false;
-      if(col<0 || col>= m)return false;
-      if(vis[row][col])return false;
-      if(grid[row][col]==0)return false;
-      return true;
-    }
-
-
-    int orangesRotting(vector<vector<int>> &grid) {
+    int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int rotten = 0;
         int totalOranges = 0;
+        int rottenOranges = 0;
         int time = 0;
-        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
-          for(int j=0;j<m;j++){
-            if(grid[i][j]==2){
-              q.push({i,j});
-              vis[i][j] = 1;
-              rotten++;
+            for(int j =0;j<m;j++){
+                if(grid[i][j]==2){
+                    rottenOranges++;
+                    q.push({i,j});
+                }
+                if(grid[i][j]!=0)totalOranges++;
             }
-            if(grid[i][j]!=0)totalOranges++;
-          }
         }
 
+        if(totalOranges==0)return 0;
 
         while(!q.empty()){
-          int k = q.size();
-           bool flag = false;
-          for(int i=0;i<k;i++){
-            pair<int,int> val = q.front();
-            q.pop();
-            int row = val.first;
-            int col = val.second;
-            if(grid[row][col]==1){
-              grid[row][col] = 2;
-            }
-            int dx[] = {0,1,0,-1} ;
-            int dy[] = {-1,0,1,0} ;
-           
-            for(int i=0;i<4;i++){
-              int nx = row + dx[i];
-              int ny = col + dy[i];
-              if(isValidFresh(nx,ny,grid,vis)){
-                q.push({nx,ny});
-                rotten++;
-                vis[nx][ny] = 1;
-                flag = true;
-              }
+            time++;
+            int size = q.size();
+            int dx[] = {0,1,0,-1};
+            int dy[] = {1,0,-1,0};
+            for(int i=0;i<size;i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nx = dx[i] + row;
+                    int ny = dy[i] + col;
+                    if(nx>=0 && nx < n && ny>=0 && ny < m && grid[nx][ny]==1){
+                        grid[nx][ny] = 2;
+                        rottenOranges++;
+                        q.push({nx,ny});
+                    }
+                }
             }
 
-          }
-          if(flag)time++;
         }
 
-
-
-        if(totalOranges-rotten>=1)return -1;
-        return time;
-
-
-
-
-
-
+        if(rottenOranges == totalOranges)return time-1;
+        return -1;
     }
 };
