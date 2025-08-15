59c271e1754c3184f846cb4e366d0a58d4ace0a0
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-15 12:14:40 +0530

    Time: 2 ms (78.32%), Space: 17.5 MB (97.35%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,50 +1,43 @@
 class Solution {
 
 private:
-   vector<int>topoSort(int n,vector<int>adj[]){
-        vector<int>ans;
-        vector<int>inDegree(n,0);
-        for(int i=0;i<n;i++){
-            for(auto & it : adj[i]){
-                inDegree[it]++;
-            }
-        }
-
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(inDegree[i]==0){
-                q.push(i);
-            }
-        }
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto & it :adj[node]){
-                inDegree[it]--;
-                if(inDegree[it]==0){
-                    q.push(it);
-                }
-            }
-
-        }
-
-        return ans;
-    }
-
 
 
 public:
     vector<int> findOrder(int N, vector<vector<int>>& arr) {
         vector<int>adj[N];
-        for(auto & it : arr){
-            adj[it[0]].push_back(it[1]);
+        vector<int>indegree(N);
+        
+          for(auto & it : arr){
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+          }
+        
+
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+          if(indegree[i]==0){
+            q.push(i);
+          }
         }
-        vector<int>topo = topoSort(N,adj);
-        reverse(topo.begin(),topo.end());
-        if(topo.size()<N)return {};
-        return topo ;
+
+        vector<int>ans;
+        while(!q.empty()){
+          int node = q.front();q.pop();
+          ans.push_back(node);
+          for(auto & it : adj[node]){
+            indegree[it]--;
+            if(indegree[it]==0){
+              q.push(it);
+            }
+          }
+        }
+
+
+        
+        if(ans.size()<N)return {};
+        return ans;
+
         
     }
 };
