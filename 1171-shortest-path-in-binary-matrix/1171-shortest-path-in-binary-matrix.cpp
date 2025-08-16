2eae9e3df33b8ae999b51fa1b1eeb5af8103a6d9
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-16 11:02:29 +0530

    Time: 15 ms (77.64%), Space: 26.3 MB (41.05%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,29 +3,28 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        if(grid[0][0]==1 || grid[n-1][m-1])return -1;
-        queue<pair<pair<int,int>,int>>q;
-        q.push({{0,0},1});
+        if(grid[0][0]!=0)return -1;
+        queue<pair<int,pair<int,int>>>q;
+        q.push({1,{0,0}});
         vector<vector<int>>vis(n,vector<int>(m,0));
         vis[0][0] = 1;
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dist = q.front().second;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+            int step = q.front().first;
+            if(row == n-1 && col == m-1)return step;            
             q.pop();
-            if(row == n-1 && col == m-1)return dist;
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     int nx = row + i;
                     int ny = col + j;
-                    if(nx>=0 && nx < n && ny >=0 && ny< m && grid[nx][ny]==0 && !vis[nx][ny]){
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny] && grid[nx][ny] == 0 ){
+                        q.push({step+1,{nx,ny}});
                         vis[nx][ny] = 1;
-                        q.push({{nx,ny},dist+1});
                     }
                 }
             }
         }
         return -1;
-
     }
 };
