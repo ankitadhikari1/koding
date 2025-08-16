c0f19233ec6f396892ca853256763e383400efa4
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-16 11:39:21 +0530

    Time: 52 ms (60.72%), Space: 24.4 MB (90.77%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -3,45 +3,36 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
+
+       
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
         dist[0][0] = 0;
         pq.push({0,{0,0}});
-
-        int dx[] = {1,0,-1,0};
-        int dy[] = {0,1,0,-1};
-
-
+        
+        int dx[] = {0,1,0,-1};
+        int dy[] = {1,0,-1,0};
+        
         while(!pq.empty()){
-            auto  node = pq.top();
-            int diff = node.first;
-            int row = node.second.first;
-            int col = node.second.second;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            int diff = pq.top().first;
             pq.pop();
-
-            if(row== n-1 && col == m-1){
-                return diff;
-            }
-
+            if(row == n-1 && col == m-1) return diff; 
 
             for(int i=0;i<4;i++){
                 int nx = row + dx[i];
                 int ny = col + dy[i];
                 if(nx>=0 && nx<n && ny>=0 && ny<m){
-                    int currDiff = max(abs(heights[row][col]-heights[nx][ny]),diff);
-                    if(currDiff<dist[nx][ny]){
-                        dist[nx][ny] = currDiff;
-                        pq.push({currDiff,{nx,ny}});
+                    int newEffort = max(diff, abs(heights[nx][ny] - heights[row][col]));
+                    if(newEffort < dist[nx][ny]){
+                        dist[nx][ny] = newEffort;
+                        pq.push({newEffort,{nx,ny}});
                     }
-                    
                 }
             }
-
-
-            
-
         }
-        return -1;
-
+        return -1; 
     }
 };
