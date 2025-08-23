ed1eb393c1529ee8856a01efe7daad4950063240
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-23 12:32:49 +0530

    Time: 430 ms (36.01%), Space: 214.6 MB (36.74%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,30 @@
-class Solution{
+class Solution {
 public:
-
-    int func(int ind , int allowed , int limit , vector<int>&arr,vector<vector<vector<int>>>&dp){
-        if(ind == arr.size() || limit == 0){
-            return 0;
-        }
-
-        if(dp[ind][allowed][limit]!=-1)return dp[ind][allowed][limit];
-
-        int profit = 0;
-        if(allowed){
-            profit = max(func(ind+1,1,limit,arr,dp), -arr[ind] + func(ind+1,0,limit,arr,dp));
-        }
-        else{
-            profit = max(func(ind+1,0,limit,arr,dp),arr[ind]+func(ind+1,1,limit-1,arr,dp));
-        }
-
-        return dp[ind][allowed][limit] = profit;
-    }
-
-
-    int maxProfit(vector<int> arr){
+    int maxProfit(vector<int> arr) {
         int n = arr.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return func(0,1,2,arr,dp);
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int allowed = 0; allowed <= 1; allowed++) {
+                for (int limit = 0; limit < 3; limit++) {
+                    int profit = 0;
+                    if (allowed) {
+                        profit = max(dp[ind + 1][1][limit],
+                                     -arr[ind] + dp[ind + 1][0][limit]);
+                    } else {
+                        profit = dp[ind + 1][0][limit];
+                        if (limit > 0) {
+                            profit = max(profit,
+                                         arr[ind] + dp[ind + 1][1][limit - 1]);
+                        }
+                    }
+
+                    dp[ind][allowed][limit] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
