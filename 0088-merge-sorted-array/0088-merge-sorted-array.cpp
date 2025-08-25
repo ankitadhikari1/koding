931c44926701f7c31ee46a5e83b20c098379aec9
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-26 00:29:24 +0530

    Time: 0 ms (100%), Space: 12.4 MB (38.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,16 +5,13 @@ public:
         int j = n-1;
         int k = m+n-1;
         while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
+            if(nums1[i]>=nums2[j]){
                 nums1[k--] =nums1[i--]; 
             }
             else if(nums1[i]<nums2[j]){
                 nums1[k--] = nums2[j--];
             }
-            else{
-                nums1[k--] = nums1[i--];
-                nums1[k--] = nums2[j--];
-            }
+           
         }
         while(i>=0){
             nums1[k--] = nums1[i--];
