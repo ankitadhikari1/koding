410f51a43864ec929fdd296828283763e1a6d232
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-25 21:05:50 +0530

    Time: 1 ms (46.69%), Space: 12.8 MB (25.6%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-
-    void func(int ind,vector<vector<int>>&ans,vector<int>&temp,vector<int>&nums){
-        if(ind==nums.size()){
+     void generateAllPowerSet(int ind, vector<int>&temp,vector<vector<int>>&ans,vector<int>&nums){
+        if(ind == nums.size()){
             ans.push_back(temp);
             return ;
         }
 
+        generateAllPowerSet(ind+1,temp,ans,nums);
         temp.push_back(nums[ind]);
-        func(ind+1,ans,temp,nums);
+        generateAllPowerSet(ind+1,temp,ans,nums);
         temp.pop_back();
-        func(ind+1,ans,temp,nums);
-
     }
 
 
-
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int> > subsets(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>temp;
-        func(0,ans,temp,nums);
+        generateAllPowerSet(0,temp,ans,nums);
         return ans;
     }
 };
