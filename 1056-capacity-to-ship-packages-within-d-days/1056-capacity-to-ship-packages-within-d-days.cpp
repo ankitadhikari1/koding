39a29136f81fd7c3d1b33eb6270593a0942c6cc1
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-08-28 23:00:12 +0530

    Time: 23 ms (5.05%), Space: 35.1 MB (14.06%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    bool func(int mid,int days,vector<int>&weights){
+        int cnt = 1;
+        int sum = 0;
+        for(int i=0;i<weights.size();i++){
+            if(weights[i]>mid)return false;
+            if(sum+weights[i]>mid){
+                cnt++;
+                sum=0;
+                if(cnt>days)return false;
+            }
+            sum+=weights[i];
+            
+        }
+        return true;
+    }
+
+
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *min_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
+        int ans = high;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(func(mid,days,weights)){
+                ans = mid ;
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
