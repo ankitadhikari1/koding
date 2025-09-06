4f0e8dd76df74d3fb9ef6f88819153abd79b5c4c
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-09-07 00:17:24 +0530

    Time: 11 ms (63.29%), Space: 25.3 MB (84.39%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<pair<int,string>> arr;
-        for (int i = 0; i < names.size(); i++) {
-            arr.push_back({heights[i], names[i]});
-        }
+        vector<int> idx(names.size());
+        iota(idx.begin(), idx.end(), 0); 
 
-       
-        sort(arr.begin(), arr.end(), [](auto &a, auto &b) {
-            return a.first > b.first;
+        sort(idx.begin(), idx.end(), [&](int a, int b) {
+            return heights[a] > heights[b]; 
         });
 
-        for (int i = 0; i < heights.size(); i++) {
-            names[i] = arr[i].second;
-        }
-        return names;
+        vector<string> result;
+        for (int i : idx) result.push_back(names[i]);
+        return result;
     }
 };
