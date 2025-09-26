907d819a92ad772c5e3e00ec27242a45e342f9c9
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-09-26 15:53:03 +0530

    Time: 47 ms (86.1%), Space: 182.2 MB (59.58%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,58 +1,56 @@
 class NumArray {
     vector<int>seg;
     int n;
-
 public:
     NumArray(vector<int>& nums) {
-         n = nums.size();
+        n = nums.size();
         seg.assign(4*n,0);
-        build(0,0,n-1,seg,nums);
+        build(0,0,n-1,nums);
+        
     }
 
-    void build(int idx , int low , int high , vector<int>&seg , vector<int>&arr){
+    void build(int idx , int low , int high , vector<int>&nums){
         if(low == high){
-            seg[idx] = arr[low];
-            return ;
-        }
-        int mid = low + (high - low)/2;
-        build(2*idx+1,low,mid,seg,arr);
-        build(2*idx+2,mid+1,high,seg,arr);
-
-        seg[idx] = seg[2*idx+1] + seg[2*idx+2];
-
-
-    }
-
-    void update(int idx , int low , int high , int index , int val ){
-        if(low == high){
-            seg[idx] = val;
+            seg[idx] = nums[low];
             return;
         }
         int mid = low + (high - low)/2;
+        build(2*idx+1,low,mid,nums);
+        build(2*idx+2,mid+1,high,nums);
+
+        seg[idx] = seg[2*idx+1]+seg[2*idx+2];
+
+    }
+
+    void update(int idx , int low , int high , int index , int val){
+        if(low == high){
+            seg[idx] = val;
+            return ;
+        }
+        int mid = low + (high - low)/2;
         if(index <= mid){
-            update(2*idx+1,low,mid,index , val);
+            update(2*idx+1,low,mid,index,val);
         }
         else{
             update(2*idx+2,mid+1,high,index,val);
         }
 
-        seg[idx] = seg[2*idx+1]+seg[2*idx+2];
+        seg[idx] = seg[2*idx+1] + seg[2*idx+2];
     }
     
     void update(int index, int val) {
         update(0,0,n-1,index,val);
-        
     }
     
-    int sumRange(int idx , int low , int high , int l , int r ){
-        if(low > r || high < l){
+    int sumRange(int idx , int low , int high , int left , int right){
+        if(low > right || high < left){
             return 0;
         }
-        else if( low >= l && high <= r){
+        if(low >= left && high <= right){
             return seg[idx];
         }
         int mid = low + (high - low)/2;
-        return sumRange(2*idx+1,low ,mid,l,r)+sumRange(2*idx+2,mid+1,high,l,r);
+        return sumRange(2*idx+1,low,mid,left,right)+sumRange(2*idx+2,mid+1,high,left,right);
     }
 
     int sumRange(int left, int right) {
