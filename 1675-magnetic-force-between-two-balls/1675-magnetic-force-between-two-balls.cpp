a802542faeeb5e4ce63030cc44a53bcef7ec1939
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-11-06 09:08:18 +0530

    Time: 45 ms (73.87%), Space: 61.8 MB (34.44%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
 
- int func(vector<int>&nums,int k , int dist){
+    bool func(vector<int>&nums,int k , int dist){
         int last = nums[0];
         int cnt = 1;
         for(int i=1;i<nums.size();i++){
             if(nums[i]-last>=dist){
-              cnt++;
-              last = nums[i];
+                cnt++;
+                last = nums[i];
             }
         }
-
-        return cnt >= k; 
+        return cnt>=k;
     }
 
 
 
-
-    int maxDistance(vector<int>& nums, int k) {
-         sort(nums.begin(),nums.end());
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
         int low = 1;
-        int high = *max_element(nums.begin(),nums.end());
+        int high = *max_element(position.begin(),position.end());
         while(low<=high){
-            int mid = low + (high - low)/2;
-            int flag = func(nums,k,mid);
+            int mid = low + (high-low)/2;
+            bool flag = func(position,m,mid);
             if(flag){
-              low = mid + 1;
+                low = mid + 1;
             }
             else{
-              high = mid -1;
+                high = mid - 1;
             }
         }
-
-
         return high;
     }
 };
