4f6415d1041a45b5a5e419372800cab80584bac6
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-11-15 19:18:29 +0530

    Time: 24 ms (31.36%), Space: 34.7 MB (21.95%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(auto & it : nums){
+            mp[it]++;
+        }
+        vector<int>ans;
+        for(auto & it : mp){
+            if(it.second==2){
+                ans.push_back(it.first);
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(mp[i]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
