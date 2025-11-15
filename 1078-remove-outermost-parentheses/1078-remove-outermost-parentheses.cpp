8df15f4fd5d702cdde6234e6ecdd12f51ab2b39c
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-11-15 12:49:42 +0530

    Time: 0 ms (100%), Space: 9 MB (53.67%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int count1 = 0, count2 = 0;
-        string out;
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                count1++;
-            } else {
-                count2++;
+        int cnt = 0;
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                if(cnt==0){
+                    cnt++;
+                }
+                else{
+                    ans+=s[i];
+                    cnt++;
+                }
             }
-
-            if (count1 == count2) {
-                count1 = 0;
-                count2 = 0;
+            else if(s[i]==')'){
+                if(cnt==1){
+                    cnt--;
+                }
+                else{
+                    ans+=s[i];
+                    cnt--;
+                }
             }
-
-            if (count1 != count2 && count1 != 1) {
-                out += s[i];
+            else{
+                ans+=s[i];
             }
         }
-
-        return out;
+        return ans;
     }
 };
