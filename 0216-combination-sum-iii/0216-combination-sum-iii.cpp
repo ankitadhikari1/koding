52a3e018ca49d0c295738848678c12299aa22bc6
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-11-28 17:31:36 +0530

    Time: 0 ms (100%), Space: 9 MB (32.27%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void func(int ind,int k , int n, vector<vector<int>>&ans,vector<int>&temp,int cnt){
-        if(n==0 && cnt == k){
-            ans.push_back(temp);
-            return;
+
+    void func(int ind , int k , int n  , vector<int>&temp ,vector<vector<int>>&ans,int sum ){
+        if(ind > 9){
+            if(sum == n && k == 0){
+                ans.push_back(temp);
+            }
+            return ;
         }
-        if(cnt>k)return;
-        if(ind>9)return;
+
+
         temp.push_back(ind);
-        func(ind+1,k,n-ind,ans,temp,cnt+1);
+        func(ind+1,k-1,n,temp,ans,sum+ind);
         temp.pop_back();
-        func(ind+1,k,n,ans,temp,cnt);
+        func(ind+1,k,n,temp,ans,sum);
     }
-
-
 
 
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>ans;
         vector<int>temp;
-        func(1,k,n,ans,temp,0);
+        func(1,k,n,temp,ans,0);
         return ans;
     }
 };
