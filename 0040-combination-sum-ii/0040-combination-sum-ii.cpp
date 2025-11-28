d3d93da555b72f0d2f0387db17e137d4b5da20a9
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-11-28 17:13:57 +0530

    Time: 3 ms (39.06%), Space: 14.1 MB (36.35%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
 
-    void func(int ind,vector<int>&nums,int k, vector<vector<int>>&ans,vector<int>&temp){
-        if(k==0){
-            ans.push_back(temp);
+        void func(int ind , vector<int>&candidates,int target ,  vector<int>&temp , vector<vector<int>>&ans){
+        if(ind == candidates.size()){
+            if(target==0){
+                ans.push_back(temp);
+            }
             return;
         }
-        if(ind == nums.size())return;
-        
-        if(k-nums[ind]>=0){
-            temp.push_back(nums[ind]);
-            func(ind+1,nums,k-nums[ind],ans,temp);
+
+        if(candidates[ind]<=target){
+            temp.push_back(candidates[ind]);
+            func(ind+1,candidates,target-candidates[ind],temp,ans);
             temp.pop_back();
         }
-        int i;
-        for(i=ind+1;i<nums.size();i++){
-            if(nums[i]!=nums[ind]){
+
+        int i ;
+        for(i = ind + 1;i<candidates.size();i++){
+            if(candidates[i]!=candidates[ind]){
                 break;
             }
         }
-        func(i,nums,k,ans,temp);
+        func(i,candidates,target,temp,ans);
+        
     }
 
 
 
 
-    vector<vector<int>> combinationSum2(vector<int>& nums, int k) {
-        
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>>ans;
         vector<int>temp;
-        func(0,nums,k,ans,temp);
+        func(0,candidates,target,temp,ans);
         return ans;
-    
     }
 };
