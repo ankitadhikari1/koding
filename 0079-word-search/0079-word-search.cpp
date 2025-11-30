892c6e8ada2accf7cea9749b92e061ee20d65899
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2025-11-30 14:49:08 +0530

    Time: 676 ms (39.35%), Space: 11.5 MB (50.97%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
 
-    bool func(int row,int col,int ind ,vector<vector<char>>& board, string word,vector<vector<int>>&vis){
-       
+    bool func(int i , int j , string word , vector<vector<char>>&board, int ind){
         int n = board.size();
         int m = board[0].size();
-        if(word[ind]!=board[row][col])return false;
-        if(ind == word.size()-1)return true;
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,-1,0,1};
-        bool flag = false;
-         vis[row][col] = 1;
-        for(int i=0;i<4;i++){
-            int nx = row + dx[i];
-            int ny = col + dy[i];
-            if(nx>=0 && nx < n && ny>=0 && ny< m && !vis[nx][ny] ){
-                flag|=func(nx,ny,ind+1,board,word,vis);
+        
+        if (board[i][j] != word[ind]) return false;
+
+// if this was the last character and it matched -> success
+if (ind == (int)word.size() - 1) return true;
+
+        
+        char ch = board[i][j];
+        board[i][j] = '#';
+       
+        int dx[] = {1,0,-1,0};
+        int dy[] = {0,1,0,-1};
+        for(int k=0;k<4;k++){
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+            if(nx >= 0 && nx < n && ny >= 0 && ny < m){
+                bool flag = func(nx,ny,word,board,ind+1);
+                if(flag)return flag;
             }
         }
-        vis[row][col] = 0;
-        return flag;
-    }
 
+        board[i][j] = ch;
+        return false;
+    }
 
 
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        bool flag = false;
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    if(func(i,j,0,board,word,vis))return true;
+                    flag = func(i,j,word,board,0);
+                    if(flag == true){
+                        return true;
+                    }
                 }
             }
         }
