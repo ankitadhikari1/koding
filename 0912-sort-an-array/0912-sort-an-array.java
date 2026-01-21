4c6ce95b99630e3a592a4f69a9e328f60faf5c69
Ankit Adhikari <ankitadhikari4187497k@gmail.com>
2026-01-21 20:57:43 +0530

    Time: 1505 ms (6.93%), Space: 85.1 MB (42.31%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,51 @@
+class Solution {
+
+        private void swap(int []nums , int i ,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    private int findPivot(int []nums , int low ,int high){
+
+        int randomIndex = low + new Random().nextInt(high - low+1);
+
+        swap(nums,low,randomIndex);
+
+
+        int pivot = nums[low];
+        int i = low ;
+        int j = high;
+
+        while(i<j){
+            while(nums[i]<= pivot && i<=high-1){
+                i++;
+            }
+            while(nums[j]>pivot && j>=low+1){
+                j--;
+            }
+            if(i<j){
+                swap(nums,i,j);
+            }
+        }
+
+        swap(nums,low,j);
+        return j;
+
+    }
+
+    private void quickSortFunc(int []nums , int low , int high){
+        if(low>=high){
+            return;
+        }
+        int pivot = findPivot(nums,low,high);
+        quickSortFunc(nums,low,pivot-1);
+        quickSortFunc(nums,pivot+1,high);
+    }
+
+    public int[] sortArray(int[] nums) {
+        quickSortFunc(nums,0,nums.length-1);
+        return nums;
+    }
+}
